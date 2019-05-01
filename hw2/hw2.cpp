4290419bb9df7ba9ba52f1206c241ae93df0daaa
Brian Jackson <bjack205@gmail.com>
2019-05-01 10:53:48 -0700

    hw2

--- hw2/hw2.cpp
+++ hw2/hw2.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
 
 #define QUESTION_1   1
 #define QUESTION_2   2
@@ -72,6 +73,32 @@ int main() {
 	timer.setLoopFrequency(1000); 
 	double start_time = timer.elapsedTime(); //secs
 	bool fTimerDidSleep = true;
+	
+	// Initialization
+	VectorXd Kp = VectorXd::Constant(dof,400);
+	Kp(6) = 50;
+	cout << Kp.transpose() << endl;
+
+	VectorXd Kv = VectorXd::Constant(dof,50);
+
+	VectorXd qdes = initial_q;
+	qdes(6) = 0.1;
+
+	Vector3d x;
+	Vector3d xd;
+	Vector3d xdes(0.3, 0.1, 0.5);
+	Vector3d xdes0(0.3, 0.1, 0.5);
+
+	VectorXd g(dof); // Gravity vector
+	VectorXd h(dof); // Coriolis plus gravity
+
+	// Get info from user
+	// cout << "Kp: " << endl;
+	double kp7 = 0.3;
+	// cin >> kp7;
+	
+	// Set up output file
+	ofstream outfile("prob4d.txt");
 
 	redis_client.set(CONTROLLER_RUNING_KEY, "1");
 	while (runloop) {
@@ -83,39 +110,114 @@ int main() {
 		robot->_q = redis_client.getEigenMatrixJSON(JOINT_ANGLES_KEY);
 		robot->_dq = redis_client.getEigenMatrixJSON(JOINT_VELOCITIES_KEY);
 		robot->updateModel();
+		robot->coriolisPlusGravity(h);
+		robot->gravityVector(g);
+
+		robot->Jv(Jv, link_name, pos_in_link);
+		robot->taskInertiaMatrix(Lambda, Jv);
+		robot->positionInWorld(x, link_name, pos_in_link);
+		robot->linearVelocityInWorld(xd, link_name, pos_in_link);
+		robot->nullspaceMatrix(N, Jv);
+		robot->dynConsistentInverseJacobian(J_bar, Jv);
 
 		// **********************
 		// WRITE YOUR CODE AFTER
 		// **********************
-		int controller_number = QUESTION_1;  // change to the controller of the question you want : QUESTION_1, QUESTION_2, QUESTION_3, QUESTION_4, QUESTION_5
+		int controller_number = QUESTION_4;  // change to the controller of the question you want : QUESTION_1, QUESTION_2, QUESTION_3, QUESTION_4, QUESTION_5
 
 
 		// ---------------------------  question 1 ---------------------------------------
 		if(controller_number == QUESTION_1)
 		{
+			// qdes(0) = initial_q(0) + 5*M_PI/180.0;			  
+			Kv(6) = -kp7;
+			// command_torques = -Kp*(robot->_q - qdes) - Kv*robot->_dq + h;
+			// cout << robot->_q(1)*180.0/M_PI << endl;
 
-			command_torques.setZero();  
+			double kp = 400.0;      // chose your p gain
+                        double kv = 40.0;      // chose your d gain
+
+                        // VectorXd q_desired = initial_q;   // change to the desired robot joint angles for the question
+                        // q_desired(0) = M_PI/2.0;
+
+                        command_torques = (-Kp).asDiagonal()*(robot->_q - qdes) - Kv.asDiagonal()*robot->_dq + h;
+			outfile << robot->_q(6) << endl;
 		}
 
 		// ---------------------------  question 2 ---------------------------------------
 		if(controller_number == QUESTION_2)
 		{
+			double kp = 200;
+			double kv = 28.28;
+			VectorXd Kv = VectorXd::Constant(dof,10);
 
-			command_torques.setZero();
+			MatrixXd F = Lambda*(-kp*(x - xdes) - kv*xd);
+			command_torques = Jv.transpose()*F + g + N.transpose()*robot->_M*((-Kv).asDiagonal()*robot->_dq);
+			// command_torques = Jv.transpose()*F + g - Kv.asDiagonal()*robot->_dq; 
+			for (int i = 0; i <= 2; i++)
+				outfile << x(i) << ",";
+			for (int i = 0; i < dof - 1; i++)
+				outfile << robot->_q(i) << ", ";
+			outfile << robot->_q(dof-1) << endl;
 		}
 
 		// ---------------------------  question 3 ---------------------------------------
 		if(controller_number == QUESTION_3)
 		{
 
-			command_torques.setZero();
+			double kp = 200;
+			double kv = 28.28;
+			VectorXd Kv = VectorXd::Constant(dof,10);
+
+			Vector3d p = J_bar.transpose()*g;
+			MatrixXd F = Lambda*(-kp*(x - xdes) - kv*xd) + p;
+			command_torques = Jv.transpose()*F - N.transpose()*robot->_M*(Kv.asDiagonal()*robot->_dq);
+
+			// Write to file
+			for (int i = 0; i <= 2; i++)
+				outfile << x(i) << ",";
+			for (int i = 0; i < dof - 1; i++)
+				outfile << robot->_q(i) << ", ";
+			outfile << robot->_q(dof-1) << endl;
 		}
 
 		// ---------------------------  question 4 ---------------------------------------
 		if(controller_number == QUESTION_4)
 		{
+			xdes << 0.1*sin(M_PI*time), 0.1*cos(M_PI*time), 0;
+			xdes = xdes + xdes0;
+			double kp = 200;
+			double kv = 28.28;
+			double kp_q = 400;
+			double kv_q = 40;
+			VectorXd Kv = VectorXd::Constant(dof,10);
+			Vector3d p = J_bar.transpose()*g; 
+			MatrixXd F;
 
-			command_torques.setZero();
+			// Part (i)
+			F = Lambda*(-kp*(x - xdes) - kv*xd) + p;
+			command_torques = Jv.transpose()*F - N.transpose()*robot->_M*(Kv.asDiagonal()*robot->_dq);
+
+			// Part (ii)
+			F = (-kp*(x - xdes) - kv*xd) + p;
+			command_torques = Jv.transpose()*F - N.transpose()*robot->_M*(Kv.asDiagonal()*robot->_dq);
+
+			// Part (iii)
+			VectorXd q_desired = VectorXd::Zero(dof);
+			F = Lambda*(-kp*(x - xdes) - kv*xd) + p;
+			command_torques = Jv.transpose()*F + N.transpose()*robot->_M*(-kp_q*(robot->_q - q_desired) - kv_q*robot->_dq); 
+
+			// Part (iv)
+			command_torques = Jv.transpose()*F + N.transpose()*robot->_M*(-kp_q*(robot->_q - q_desired) - kv_q*robot->_dq) + N.transpose()*g; 
+
+			// Write to file
+			for (int i = 0; i <= 2; i++)
+				outfile << xdes(i) << ",";
+			for (int i = 0; i <= 2; i++)
+				outfile << x(i) << ",";
+			for (int i = 0; i < dof - 1; i++)
+				outfile << robot->_q(i) << ", ";
+			outfile << robot->_q(dof-1) << endl;
 		}
 
 		// **********************
